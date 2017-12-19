7171dcee6f68bd81fbb8e5c36009b77ccb1bfc2c
shadow <1031198582@qq.com>
2017-12-19 16:04:47 +0800

    material design

---
+++ app/src/main/java/com/example/administrator/shadowapplication/material_design/MaterialActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.shadowapplication.material_design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.shadowapplication.R;
+
+public class MaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+    }
+}
