6290f4abd4afc1401f70a2e409aee7fc7612a8f0
shadow <1031198582@qq.com>
2018-01-29 14:59:16 +0800

    动态的自定义view

--- app/src/main/java/com/example/administrator/shadowapplication/activity/CustomViewGroupActivity.java
+++ app/src/main/java/com/example/administrator/shadowapplication/activity/CustomViewGroupActivity.java
@@ -1,21 +1,31 @@
 package com.example.administrator.shadowapplication.activity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.administrator.shadowapplication.R;
+import com.example.administrator.shadowapplication.widget.CustomAnimView;
 import com.example.administrator.shadowapplication.widget.CustomView2;
+
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 public class CustomViewGroupActivity extends AppCompatActivity {
 
     private TextView three;
     private CustomView2 customSun;
+    private CustomAnimView customAnimView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+        // 去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view_group);
         three = (TextView) findViewById(R.id.tree);
@@ -33,9 +43,59 @@ public class CustomViewGroupActivity extends AppCompatActivity {
             }
         });
 
+        customAnimView = (CustomAnimView) findViewById(R.id.customAnimView);
+        new Thread(customAnimView).start();
+
     }
 
     public void toast(String content) {
         Toast.makeText(CustomViewGroupActivity.this, content, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/administrator/shadowapplication/widget/CustomAnimView.java
@@ -0,0 +1,86 @@
+package com.example.administrator.shadowapplication.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * <pre>
+ *     author : Administrator
+ *     time   : 2018/1/29
+ *     desc   :
+ * </pre>
+ */
+
+
+public class CustomAnimView extends View implements Runnable {
+    private Paint paint;
+    private int radius;
+    private Context mContext;
+    private int mWidth, mHeight;
+
+    public CustomAnimView(Context context) {
+        this(context, null);
+    }
+
+    public CustomAnimView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomAnimView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomAnimView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initView(context);
+    }
+
+    public void initView(Context context) {
+        this.mContext = context;
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        this.mWidth = w;
+        this.mHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, radius, paint);
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                if (radius < 200) {
+                    radius += 10;
+                    postInvalidate();
+                } else {
+                    radius = 0;
+                }
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
