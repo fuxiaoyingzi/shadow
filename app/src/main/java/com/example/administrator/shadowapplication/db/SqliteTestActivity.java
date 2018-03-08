3c3d9204dea3ebf2d253a46b94c06cdb5f5a7bec
shadow <1031198582@qq.com>
2018-03-08 15:31:25 +0800

    沉浸式状态栏

--- app/src/main/java/com/example/administrator/shadowapplication/db/SqliteTestActivity.java
+++ app/src/main/java/com/example/administrator/shadowapplication/db/SqliteTestActivity.java
@@ -5,6 +5,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -42,6 +44,11 @@ public class SqliteTestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_sqltest);
         saveImage = findViewById(R.id.saveImage);
         getImage = findViewById(R.id.getImage);
